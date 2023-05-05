2fa7822aa1627218111e0fa14332128046f9dad1
Tony Thayer-Osborne <anthony.thayer-osborne@curology.com>
2023-05-05 13:49:02 -0700

    Updates

--- init.lua
+++ init.lua
@@ -493,5 +493,11 @@ cmp.setup {
   },
 }
 
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
+
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
